4855a4e06909ded8ac6ca12cc705f6c7bce6e8b3
Prateek Kumar Gupta <prateekkumargupta6@gmail.com>
2026-09-03 23:05:39 +0530

    Added photo for Kajal Singh

--- src/components/TeamSection.jsx
+++ src/components/TeamSection.jsx
@@ -8,7 +8,7 @@ export default function TeamSection() {
       label: "GEN 1.0",
       year: "CLASS OF '26",
       members: [
-        { name: "Kajal Singh", role: "VICE PRESIDENT", img: "assets/vortex-logo.jpg" },
+        { name: "Kajal Singh", role: "VICE PRESIDENT", img: "assets/kajal.jpg" },
         { name: "Nischay Verma", role: "PRESIDENT", img: "assets/vortex-logo.jpg" },
         { name: "Mankiran Kaur", role: "VICE PRESIDENT", img: "assets/mankiran.jpg" }
       ]
